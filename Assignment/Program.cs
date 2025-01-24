cb2a72eb0c2d58240df008055d3b43b58aa05024
admin <admin@DESKTOP-JKD834T>
2025-01-24 19:10:57 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using Assignment.Second_Project;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region test Second_Project
+           
+            double num1 = 10;
+            double num2 = 2;
+
+            Console.WriteLine($"Add: {Math_.Add(num1, num2)}");
+            Console.WriteLine($"Subtract:{ Math_.Subtract(num1, num2)}");
+            Console.WriteLine($"Multiply: {Math_.Multiply(num1, num2)}");
+            Console.WriteLine($"Divide: {Math_.Divide(num1, num2)}");
+
+            // Testing division by zero
+            Console.WriteLine($"Divide by Zero: {Math_.Divide(num1, 0)}");
+            #endregion
+
+        }
+    }
+}
---
+++ Assignment/Second_Project/Math.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Second_Project
+{
+   public class Math_
+    {
+    
+       
+        #region method
+        public static double Add(double _var1, double _var2)
+        {  
+            return _var1+ _var2;
+        }
+        public static double Subtract(double _var1, double _var2)
+        { 
+            return _var1-_var2; 
+        }
+        public static double Multiply(double _var1, double _var2)
+        { 
+            return _var2* _var1; 
+        }
+        public static double Divide(double _var1, double _var2)
+        { if (_var2==0.0)
+            { Console.WriteLine("can not divide by zero ");
+                return double.NaN; 
+            }
+        else
+                return _var1/_var2;
+             
+        }
+        #endregion
+
+    }
+}
